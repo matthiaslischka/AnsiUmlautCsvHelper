efe5ce1b2ce5965b3f63c14590c969e2a5617d6f
Matthias Lischka <matthias.lischka@oebb.at>
2024-05-06 14:44:03 +0200

    batman commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+var csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    Encoding = Encoding.Default
+};
+
+using var reader = new StreamReader("AnsiWithUmlaut.csv", Encoding.Default);
+using var csv = new CsvReader(reader, csvConfiguration);
+var records = csv.GetRecords<CsvDto>();
+Console.ReadLine();
+
+internal class CsvDto
+{
+    [Name("id")] public string Id { get; set; }
+
+    [Name("name")] public string Name { get; set; }
+
+    [Name("währung")] public string Currency { get; set; }
+}
